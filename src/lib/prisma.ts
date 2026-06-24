2ae388c9305c2f59facb4d76be2ad893734dc6b1
Mohamed Massinissa, Ziane <e2185817@cmaisonneuve.qc.ca>
2026-06-24 12:39:49 -0400

    project init

---
+++ src/lib/prisma.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { Pool, neonConfig } from "@neondatabase/serverless";
+import ws from "ws";
+
+neonConfig.webSocketConstructor = ws;
+
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not defined");
+}
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+const adapter = new PrismaNeon({
+  connectionString,
+});
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+    log: ["error"],
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
